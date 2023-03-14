fac079d7b41777980fdf8d0824629e2b2a2ff478
Kur9ix <felixtobiaswalter@gmail.com>
2023-03-14 23:28:02 +0100

    commit;

--- HTML/Website/clock.js
+++ HTML/Website/clock.js
@@ -4,6 +4,8 @@ function displayTime(){
     var min = dateTime.getMinutes();
     var sec = dateTime.getSeconds();
 
+    
+
     document.getElementById("hours").innerHTML = hrs;
     document.getElementById("minutes").innerHTML = min;
     document.getElementById("seconds").innerHTML = sec;
---
+++ HTML/Website/menu.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+const github = document.getElementById('github-button');
+const showbutton = document.getElementById('show-button');
+const menu = document.getElementById('menu');
+
+github.addEventListener('click', function(){
+    window.location.href = 'https://github.com/Kur9ix?tab=repositories';
+});
+
+showbutton.addEventListener('click', function(){
+    if(menu.style.display === 'none'){
+        menu.style.display = 'block';
+    } else{
+        menu.style.display = 'none'
+    }
+});
+});
+
